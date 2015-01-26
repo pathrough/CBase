69378a1276bf84dc02dbd16a0af343a1adc4ad52
pathrough <1598842213@qq.com>
2015-01-26 13:05:48 +0800

    fdg

---
+++ CBase/Config.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBase
+{
+    public static sealed class Config
+    {
+        public static string INDEX_FILE_NAME = "index";
+        public static string DATA_FILE_NAME = "data";
+    }
+}
