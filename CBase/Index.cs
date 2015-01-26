58bf14ee578c632941f4a63ba146f10956fb9682
pathrough <1598842213@qq.com>
2015-01-26 14:12:48 +0800

    gff

--- CBase/Index.cs
+++ CBase/Index.cs
@@ -9,11 +9,18 @@ namespace CBase
 {
     public class Index : IByteConvertor
     {
+        //create
         public Index(IndexBlock parentBlock, string indexValue)
         {
             this.ParentBlock = parentBlock;
             this.IndexValue = indexValue;
         }
+
+        public Index(IndexBlock parentBlock, long lngPosition)
+        {
+            this.ParentBlock = parentBlock;           
+        }
+        
         /// <summary>
         /// 所属的块
         /// </summary>
@@ -30,7 +37,9 @@ namespace CBase
         /// <summary>
         /// [导航属性]指向的下级索引块
         /// </summary>
-        public IndexBlock SubBlock { get; set; }
+        public IndexBlock SubBlock {
+            get; 
+        }
 
         /// <summary>
         /// 持久化元数据
@@ -60,32 +69,73 @@ namespace CBase
     public class IndexBlock : IByteConvertor
     {
         readonly IndexFile _File;
-        public IndexFile File { get; }
+        public IndexFile IndexFile { get; }
+
+        //创建
         public IndexBlock(bool isLeaf,IndexFile file)
         {
             this._IsLeaf = isLeaf;
             this._File = file;
         }
 
-        public static IndexBlock FirstIndexBlock { get; set; }
-
-        public static IndexBlock CreateIndexBlock(string value,IndexFile file)
+        //获取
+        public IndexBlock(IndexFile file,long lngPosition)
         {
-            IndexBlock ib = new IndexBlock(isLeaf: true, file:file);
-            ib.IndexList.Add(new Index(indexValue: value, parentBlock: ib));
-            using (FileStream fs = new FileStream(Config.INDEX_FILE_NAME, FileMode.OpenOrCreate, FileAccess.ReadWrite))
-            {
-                byte[] bs = ib.GetBytes().ToArray();
-                fs.Write(bs, 0, bs.Length);
-            }
-            return ib;
+            IndexFile.File.Position = lngPosition;
+
+            byte[] bs = new byte[4];
+            IndexFile.File.Read(bs, 0, bs.Length);
+            _BlockLength = bs.ToInt32();
+
+            bs = new byte[8];
+            IndexFile.File.Read(bs, 0, bs.Length);
+            _NextBlockPosition = bs.ToInt64();
+
+            bs = new byte[1];
+            IndexFile.File.Read(bs, 0, bs.Length);
+            _IsLeaf = bs.ToBoolean();
         }
-        
-        public List<Index> IndexList { get; set; }
+
+        readonly int _BlockLength;
+
+        readonly long _NextBlockPosition;
+
+        IndexBlock _NextBlock;
+        IndexBlock NextBlock
+        {
+            get
+            {
+                if (_NextBlock==null)
+                {
+                    _NextBlock = new IndexBlock(this.IndexFile,_NextBlockPosition);
+                }
+                return _NextBlock;
+            }
+        }
+
+        //public static IndexBlock CreateIndexBlock(string value, IndexFile file)
+        //{
+        //    IndexBlock ib = new IndexBlock(isLeaf: true, file: file);
+        //    ib.IndexList.Add(new Index(indexValue: value, parentBlock: ib));
+        //    using (FileStream fs = new FileStream(Config.INDEX_FILE_NAME, FileMode.OpenOrCreate, FileAccess.ReadWrite))
+        //    {
+        //        byte[] bs = ib.GetBytes().ToArray();
+        //        fs.Write(bs, 0, bs.Length);
+        //    }
+        //    return ib;
+        //}
+
+        List<Index> _IndexList;
+        public List<Index> IndexList {
+            get
+            {
+                return _IndexList;
+            }
+        }
 
         public int IndexCount { get; set; }
 
-        public long NextIndexBlockPosition { get; set; }
+        //public long NextIndexBlockPosition { get; set; }
 
         readonly bool _IsLeaf;
         public bool IsLeaf { get;}
@@ -126,21 +176,44 @@ namespace CBase
             _FileName = fileName;
             _File = new FileStream(fileName, FileMode.OpenOrCreate, FileAccess.ReadWrite);
         }
-        List<IndexBlock> _IndexBlockList;
-        public List<IndexBlock> IndexBlockList
+
+        IndexBlock _FirstBlock;
+
+        public IndexBlock FirstBlock
         {
             get
             {
-                if(_IndexBlockList==null)
+                if (_FirstBlock == null)
                 {
-                    //todo:_IndexBlockList io
-                    if(_IndexBlockList==null)
-                    {
-                        _IndexBlockList = new List<IndexBlock>();
-                    }
+                    _FirstBlock = new IndexBlock(this, 0);
                 }
-                return _IndexBlockList;
+                return _FirstBlock;
             }
+        }
+        //List<IndexBlock> _IndexBlockList;
+        //public List<IndexBlock> IndexBlockList
+        //{
+        //    get
+        //    {
+        //        if(_IndexBlockList==null)
+        //        {
+        //            //todo:_IndexBlockList io
+        //            if(_IndexBlockList==null)
+        //            {
+        //                _IndexBlockList = new List<IndexBlock>();
+        //            }
+        //        }
+        //        return _IndexBlockList;
+        //    }
+        //}
+        
+        IndexBlock GetFirstIndexBlock()
+        {
+            File.Position = 0;
+            byte[] bs = new byte[8];
+            File.Read(bs, 0, bs.Length);
+            long blnBlockLen = bs;
+
         }
 
     }
--- CBase/Utility.cs
+++ CBase/Utility.cs
@@ -17,15 +17,28 @@ namespace CBase
         {
             return BitConverter.GetBytes(input);
         }
+        public static int ToInt32(this byte[] input)
+        {
+            return BitConverter.ToInt32(input, 0);
+        }
 
         public static byte[] ToBytes(this long input)
         {
             return BitConverter.GetBytes(input);
         }
 
+        public static long ToInt64(this byte[] input)
+        {
+            return BitConverter.ToInt64(input,0);
+        }
+
         public static byte[] ToBytes(this bool input)
         {
             return BitConverter.GetBytes(input);
+        }
+        public static bool ToBoolean(this byte[] input)
+        {
+            return BitConverter.ToBoolean(input, 0);
         }
     }
 }
